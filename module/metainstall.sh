c6a02d19063c48234086c60897f39368e4542b83
Admire <43035036+Admirepowered@users.noreply.github.com>
2026-04-20 17:05:20 +0800

    Refactor handle_partition function and calls

--- module/metainstall.sh
+++ module/metainstall.sh
@@ -4,19 +4,21 @@ export KSU_METAMODULE="magisk_mount"
 install_module
 
 handle_partition() {
-	partition="$1"
-	
-	if [ ! -d "$MODPATH/system/$partition" ]; then
-		return
-	fi
-	
-	if [ -L "/system/$partition" ] && [ -d "/$partition" ]; then
-		ui_print "- Handle partition /$partition"
-		ln -sf "./system/$partition" "$MODPATH/$partition"
-	fi
+    PARTITION="$1"
+    REQUIRE_SYMLINK="$2"
+    if [ ! -e "$MODPATH/system/$PARTITION" ]; then
+        # no partition found
+        return;
+    fi
+
+    if [ "$REQUIRE_SYMLINK" = "false" ] || [ -L "/system/$PARTITION" ] && [ "$(readlink -f "/system/$PARTITION")" = "/$PARTITION" ]; then
+        ui_print "- Handle partition /$PARTITION"
+        ln -sf "./system/$PARTITION" "$MODPATH/$PARTITION"
+    fi
 }
 
-handle_partition system_ext
-handle_partition vendor
-handle_partition product
+handle_partition vendor true
+handle_partition system_ext true
+handle_partition product true
+handle_partition odm false
 
